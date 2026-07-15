4961ed03647026359a49ed0a76d4dd74c824378e
arjung151098-netizen <arjung151098@gmail.com>
2026-07-15 22:15:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,128 @@
+
+
+
+
+
+/// array 
+
+
+// const abs = [10,20,30,40];
+
+// console.log(abs)
+
+// abs.push(100);
+// console.log(abs)
+
+
+// /// merge array 
+
+// const a= [1,2,3];
+// const b = [4,5,6]
+// const c = [...a,...b, 7,8,9];
+// console.log(c);
+
+
+
+// const sortarr = [100, 70, 60, 10,1];
+
+//  sortarr.sort((a,b) => a-b);
+
+// console.log(sortarr)
+
+
+// const arjun = ['kalli ','apple','mongo'];
+// console.log(arjun.sort());
+
+
+
+// const aio=sortarr.forEach((a) => {
+//     console.log(a);
+
+// })
+
+// const bioo=sortarr.map(a => {
+//     console.log(a *2)
+
+// })
+
+
+// const coo=sortarr.filter((a) => {
+//    console.log(a>10)
+// })
+
+
+
+// const arr = [5,4,3,2,1,100,0];
+  
+// // const a = arr.reverse();
+// // console.log(a);
+
+// let max = arr[0];
+
+// for ( let i = 0 ; i<arr.length;i++){
+
+//     if(arr[i]<max){
+//         max = arr[i];
+//     }
+// }
+// console.log(max);
+
+
+
+// ////
+
+// const a = [1,1,2,3,5,9,9];
+
+
+// const dup = new Set(a);
+
+// console.log(dup);
+
+
+// const oddoreven = [1,2,3,4,5,6];
+
+// for(let i=0 ; i<oddoreven.length;i++) {
+//     // if ( i %2 ==0) {
+//     //     console.log("it is even number" + i);
+//     // }else {
+//     //      console.log("it is odd number" + i);
+//     // }
+
+
+//     oddoreven.forEach((a) => {
+//         if(a%2 ==0) {
+//             console.log("its is even number " + a)
+//         }else {
+//             console.log("its is odd number " + a)
+//         }
+//     })
+// }
+
+
+
+const arr = [1,2,3,4,5,6];
+
+
+// arr.forEach((r) => {
+//     console.log(r)
+// });
+
+
+// for(let key in arr) {
+//     console.log("it is in function")
+//     console.log(arr[key]);
+// }
+
+
+// for(let key of arr) {
+//     console.log(key);
+// }
+
+const b = arr.filter((k) => k >=3 )
+
+console.log(b);
+
+
+
+
+
